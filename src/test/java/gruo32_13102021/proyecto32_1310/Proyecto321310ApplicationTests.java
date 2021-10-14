34cca902cff0b941205afa4b6079ed39fc533358
juan Mosquera <juan.mosquera.mt@correo.usa.edu.co>
2021-10-13 21:41:59 -0500

    springboot

---
+++ src/test/java/gruo32_13102021/proyecto32_1310/Proyecto321310ApplicationTests.java
@@ -0,0 +1,13 @@
+package gruo32_13102021.proyecto32_1310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto321310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
